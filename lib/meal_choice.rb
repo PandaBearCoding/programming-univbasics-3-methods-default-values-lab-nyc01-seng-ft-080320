745cb8ccbdc4afe89b954abcecbfa67d7d5a4a7d
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 00:41:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,19 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(veg1, veg2, protein = "tofu")
+#when using puts, the return value of this method below (lines 6-9) is nil AKA not what we want
+
+#def meal_choice(veg1, veg2, protein = "tofu")
+  #puts "What a nutritious meal!"
+  #puts "A plate of #{protein} with #{veg1}, and #{veg2}."
+#end
+
+
+protein = 'tofu')
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1}, and #{veg2}."
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal
 end
 
 meal_choice("carrots", "beans", " chicken")
